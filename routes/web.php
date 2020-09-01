8ee8115bd3aa9b67e9a51e2b039424b95febbe47
Iago Queiroz <iago.queiroz@hotmail.com>
2020-09-01 08:39:28 +0000

    Remove route to test PDF

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,3 @@ Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/mycourse/{course}/get-certification', 'HomeController@getCertification')->name('get-certification');
-
-Route::get('certification', function() {
-    $course = App\Course::find(5);
-    $user = App\User::find(1);
-
-    return \PDF::loadView('certification', ['course' => $course, 'user' => $user])->stream();
-});
